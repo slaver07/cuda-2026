4963343ef8fa024bdd34b5fd9c40b172e72d4379
Maxim <scourgetp@gmail.com>
2026-05-09 15:17:48 +0300

    [TASK3] A lot of optimization

--- 3823B1FI3/3_naive_gemm_omp/potashnik_maxim/naive_gemm_omp.cpp
+++ 3823B1FI3/3_naive_gemm_omp/potashnik_maxim/naive_gemm_omp.cpp
@@ -1,12 +1,34 @@
+#ifdef __GNUC__
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC target("avx,avx2,fma")
+#endif
+
+/* Optimizations
+1. Parralelism, SIMD, Flags
+2. Better cache usage (IJK -> IKJ)
+3. Calculating a[i][k] out of j-loop
+4. SIMD in inner loop
+5. Using pointers to (MAYBE) simplify data access
+*/
+
 #include "naive_gemm_omp.h"
 
 std::vector<float> NaiveGemmOMP(const std::vector<float>& a, const std::vector<float>& b, int n) {
     std::vector<float> c(n * n);
 
+#pragma omp parallel for schedule(static)
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            for (int k = 0; k < n; k++) {
-                c[i * n + j] += a[i * n + k] * b[k * n + j]; 
+        for (int k = 0; k < n; k++) {
+            float a_ik = a[i * n + k]; 
+            float* c_i = &c[i * n];
+            const float* b_k = &b[k * n];
+
+#ifdef __GNUC__
+#pragma omp simd
+#endif
+            for (int j = 0; j < n; j++) {
+                c_i[j] += a_ik * b_k[j];
             }
         }
     }
