131301779b53f77709884c524e53201726543534
Max <scourgetp@gmail.com>
2026-05-09 12:19:05 +0300

    [TASK1] Thread parallelism, SIMD, sigmoid version of formula, added flags for GCC

--- 3823B1FI3/1_gelu_omp/potashnik_maxim/gelu_omp.cpp
+++ 3823B1FI3/1_gelu_omp/potashnik_maxim/gelu_omp.cpp
@@ -1,35 +1,38 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx2, fma")
+
 #include <vector>
 #include <cmath>
+#include <omp.h>
 
 #include "gelu_omp.h"
 
 /* Optimizations
 
-1. Optimized calculations
-tanh(x) = (e^x - e^(-x)) / (e^x + e^(-x)) = 1 - 2 / (e^(2*x) + 1) 
+1. Optimized calculations (sigmoid form) 
 2. Inlined function into loop
-
+3. Thread parallelism + SIMD for each thread
+4. Flags
 */
 
-constexpr float sqrt_2_over_pi = 0.7978845608028654f;
-constexpr float coeff = 0.044715f;
-
+constexpr float coeff_1 = 1.59576912f;
+constexpr float coeff_2 = 0.044715f;
 
 std::vector<float> GeluOMP(const std::vector<float>& input)
 {
     int sz = static_cast<int>(input.size());
     std::vector<float> res(sz);
     
+#pragma omp parallel for simd schedule(static)
     for (int i = 0; i < sz; i++)
     {
         float x = input[i];
 
         float x3 = x * x * x;
-        float exp_arg = 2 * sqrt_2_over_pi * (x + coeff * x3);
-        float exp_val = expf(exp_arg);
-        float tanh_val = 1 - 2 / (exp_val + 1);
-
-        res[i] = 0.5 * x * (1.0f + tanh_val);
+        float exp_arg = coeff_1 * (x + coeff_2 * x3);
+        float sigmoid = 1.0f / (1.0f + expf(-exp_arg));
+        
+        res[i] = x * sigmoid;
     }
     return res;
 }
