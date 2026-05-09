e97ae61c13af34118c9221c8c99fc6374058f183
Max <scourgetp@gmail.com>
2026-05-09 11:42:53 +0300

    [TASK1] Moved from tanh to exp

--- 3823B1FI3/1_gelu_omp/potashnik_maxim/gelu_omp.cpp
+++ 3823B1FI3/1_gelu_omp/potashnik_maxim/gelu_omp.cpp
@@ -3,13 +3,20 @@
 
 #include "gelu_omp.h"
 
+/* Optimizations
+
+1. Optimized formula
+tanh(x) = (e^x - e^(-x)) / (e^x + e^(-x)) = 1 - 2 / (e^(2*x) + 1) 
+
+*/
 
 float GELU(const float& x) {
     constexpr float sqrt_2_over_pi = 0.7978845608028654f;
     constexpr float coeff = 0.044715f;
     float x3 = x * x * x;
-    float tanh_arg = sqrt_2_over_pi * (x + coeff * x3);
-    float tanh_val = std::tanh(tanh_arg);
+    float exp_arg = 2 * sqrt_2_over_pi * (x + coeff * x3);
+    float exp_val = expf(exp_arg);
+    float tanh_val = 1 - 2 / (exp_val + 1);
     return 0.5 * x * (1.0f + tanh_val);
 } 
 
